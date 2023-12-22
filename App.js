158bf8d85457fe3457f107c45a25a22c6b5f2fd1
AnitaPannerselvam <125266023+AnitaPannerselvam@users.noreply.github.com>
2023-12-22 11:52:09 +0530

    I have done a project of customize gift portal which includes login signup and it also includes navbar

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
+import React from 'react';
 import './App.css';
-import LoginSignup from './Components/Assets/LoginSignup/LoginSignup';
+import { BrowserRouter as Router ,Routes ,Route } from 'react-router-dom';
+import Navbar from './Components/Navbar';
+import Home from './Pages/Home';
+import Products from './Pages/Products';
+import Contactus from './Pages/Contactus';
+import LoginSignup from './Pages/LoginSignup';
+import Notfound from './Pages/Notfound'
+
 function App() {
   return (
     <div>
-  <LoginSignup/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/"  element={<Home/>} />
+          <Route path="/Products" element={<Products/>} />
+          <Route path="/Contactus" element={<Contactus/>} />
+          <Route path="/LoginSignup" element={<LoginSignup/>} />
+          <Route component={Notfound} />
+          </Routes>
+      </Router>
     </div>
   );
 }
